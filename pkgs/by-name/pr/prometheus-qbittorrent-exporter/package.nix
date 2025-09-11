cc13babaf36bfa21a70aaef452892b3d438d22b4
bas <github.plated100@passmail.net>
2025-09-11 15:31:42 +0200

    prometheus-qbittorrent-exporter: init at 1.11.0

---
+++ pkgs/by-name/pr/prometheus-qbittorrent-exporter/package.nix
@@ -0,0 +1,35 @@
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub
+}:
+
+buildGoModule rec {
+  pname = "prometheus-qbittorrent-exporter";
+  version = "1.11.0";
+
+  src = fetchFromGitHub {
+    owner = "martabal";
+    repo = "qbittorrent-exporter";
+    rev = "v${version}";
+    hash = "sha256-Rv1UuvWfQzHQ82ZKfLWnxhCWYhALy3CuLL6nUzeNugc=";
+  };
+
+  sourceRoot = "${src.name}/src";
+
+  vendorHash = "sha256-21/L4etH/xi3q69FCsYxAKui4PhPN1c+vZt3ZNnI0+8=";
+
+  ldflags = [
+    "-s"
+    "-w"
+    "-X 'qbit-exp/app.version=v${version}'"
+  ];
+
+  meta = {
+    description = "A fast and lightweight prometheus exporter for qBittorrent";
+    homepage = "https://github.com/undefined-landmark/qbittorrent-exporter";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ undefined-landmark ];
+    mainProgram = "prometheus-qbittorrent-exporter";
+  };
+}
