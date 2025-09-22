b74f50561c8f1f32774d3bbb6a2ff19e96554723
bas <github.plated100@passmail.net>
2025-09-22 18:00:48 +0200

    pkg: temporarily use master

--- pkgs/by-name/pr/prometheus-qbittorrent-exporter/package.nix
+++ pkgs/by-name/pr/prometheus-qbittorrent-exporter/package.nix
@@ -6,22 +6,25 @@
 
 buildGoModule rec {
   pname = "prometheus-qbittorrent-exporter";
-  version = "1.11.0";
+  version = "1.11.0-FILE";
 
   src = fetchFromGitHub {
     owner = "martabal";
     repo = "qbittorrent-exporter";
-    rev = "v${version}";
-    hash = "sha256-Rv1UuvWfQzHQ82ZKfLWnxhCWYhALy3CuLL6nUzeNugc=";
+    rev = "d6aef64118278b6b4aa0b200359c3bc2f772f8f3";
+    hash = "sha256-I9qq9GsN8RPI8qVLetb71AtO49j2dVA+Kpp7INTWJSU=";
   };
+
+  postPatch = ''
+    substituteInPlace go.mod --replace-fail 'go 1.25.1' 'go 1.25'
+  '';
 
   sourceRoot = "${src.name}/src";
 
-  vendorHash = "sha256-21/L4etH/xi3q69FCsYxAKui4PhPN1c+vZt3ZNnI0+8=";
+  vendorHash = "sha256-jJmhRnjioeTq9Uol0lYLChPi4O1D9JnGqN7q1XK36yE=";
 
   ldflags = [
     "-s"
-    "-w"
     "-X 'qbit-exp/app.version=v${version}'"
   ];
 
