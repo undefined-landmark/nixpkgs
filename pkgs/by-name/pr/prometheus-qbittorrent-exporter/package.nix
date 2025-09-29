29c17bf2cb4e9ba525a1fc82fcc45fecfce5a9d1
bas <github.plated100@passmail.net>
2025-09-29 09:25:51 +0200

    Add hashes

--- pkgs/by-name/pr/prometheus-qbittorrent-exporter/package.nix
+++ pkgs/by-name/pr/prometheus-qbittorrent-exporter/package.nix
@@ -7,18 +7,18 @@
 
 buildGoModule rec {
   pname = "prometheus-qbittorrent-exporter";
-  version = "1.12.0";
+  version = "1.12.1";
 
   src = fetchFromGitHub {
     owner = "martabal";
     repo = "qbittorrent-exporter";
     tag = "v${version}";
-    hash = "";
+    hash = "sha256-9J4nGG52M7SSeXigLBJK/dqXRvSpPqOGRJ8BQx7+1eU=";
   };
 
   sourceRoot = "${src.name}/src";
 
-  vendorHash = "";
+  vendorHash = "sha256-jJmhRnjioeTq9Uol0lYLChPi4O1D9JnGqN7q1XK36yE=";
 
   ldflags = [
     "-s"
